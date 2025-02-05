885cbbc9e49b59a24340c996c38faec935a82cad
José Lisboa Filho <jlisboa600@gmail.com>
2025-02-04 23:52:29 -0300

    Criação do Backend SpringBoot Java

---
+++ Backend/students_activity/src/main/java/com/students_activity/students_activity/StudentsActivityApplication.java
@@ -0,0 +1,13 @@
+package com.students_activity.students_activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsActivityApplication.class, args);
+	}
+
+}
---
+++ Backend/students_activity/src/test/java/com/students_activity/students_activity/StudentsActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.students_activity.students_activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
