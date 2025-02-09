491d508e6bc3bb0712c5ffd2e4746e053bccf14a
José Lisboa Filho <jlisboa600@gmail.com>
2025-02-09 00:18:22 -0300

    Propriedades Alteradas e Controllers Melhorados

--- Backend/students_activity/src/main/java/com/students_activity/students_activity/resources/CourseController.java
+++ Backend/students_activity/src/main/java/com/students_activity/students_activity/resources/CourseController.java
@@ -3,10 +3,19 @@ package com.students_activity.students_activity.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.students_activity.students_activity.models.Course;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @CrossOrigin
@@ -19,4 +28,21 @@ public class CourseController {
         new Course(5, "Física", 5),
         new Course(6, "Idéias", 6),
         new Course(7, "Música", 7));
+
+    @PostConstruct
+    public void init() {
+        //SI
+    }
+    @GetMapping("students")
+    public List<Course> getCategories(){
+        return courses;
+    }
+    public ResponseEntity<Course> getCourse(@PathVariable int id){
+        Course cs = courses.stream()
+        .filter(s -> s.getId() == id)
+        .findFirst()
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course Not Found"));
+
+        return ResponseEntity.ok(cs);
+    }
 }
