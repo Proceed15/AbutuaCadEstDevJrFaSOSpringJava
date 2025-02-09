cbe4a2854fea7be764a64c0deb6cc2d71ff2b98e
José Lisboa Filho <jlisboa600@gmail.com>
2025-02-08 23:59:31 -0300

    Melhoria nos Controller Student e Course

--- Backend/students_activity/src/main/java/com/students_activity/students_activity/resources/CourseController.java
+++ Backend/students_activity/src/main/java/com/students_activity/students_activity/resources/CourseController.java
@@ -1,5 +1,22 @@
 package com.students_activity.students_activity.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.students_activity.students_activity.models.Course;
+
+@RestController
+@CrossOrigin
 public class CourseController {
-    
+    private List<Course> courses = Arrays.asList(
+        new Course(1, "Desenvolvimento de Sistemas", 1),
+        new Course(2, "Fogos de Artíficio", 2),
+        new Course(3, "Psicologia", 3),
+        new Course(4, "Administração", 4),
+        new Course(5, "Física", 5),
+        new Course(6, "Idéias", 6),
+        new Course(7, "Música", 7));
 }
--- Backend/students_activity/src/main/java/com/students_activity/students_activity/resources/StudentController.java
+++ Backend/students_activity/src/main/java/com/students_activity/students_activity/resources/StudentController.java
@@ -1,5 +1,62 @@
 package com.students_activity.students_activity.resources;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.students_activity.students_activity.models.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@CrossOrigin
 public class StudentController {
+
+    private List<Student> students = new ArrayList<>();
+    @PostConstruct
+    public void init(){
+        //Object[] students={'a', '1', '#'};
+    }
+    @PostMapping("students")
+    public ResponseEntity<Student>save(@RequestBody Student student) {
+        //TODO: process POST request
+        student.setId(students.size()+1);
+        students.add(student);
+
+        URI location = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(student.getId())
+        .toUri();
+
+        return ResponseEntity.created(location).body(student);
+    }
     
+    @GetMapping("products/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable int id){
+        Student std = students.stream()
+        .filter(s-> s.getId() == id)
+        .findFirst()
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student Not Found"));
+
+        return ResponseEntity.ok(std);
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return students;
+    }
 }
